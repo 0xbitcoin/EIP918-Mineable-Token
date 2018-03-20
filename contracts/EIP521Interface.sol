de4232c5114695eb08c0bcfbf65e63f25809918f
Andy M <admazzola@gmail.com>
2018-03-20 13:50:44 -0400

    adding contracts

---
+++ contracts/EIP521Interface.sol
@@ -0,0 +1,19 @@
+// Abstract contract for the full ERC 20 Token standard
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+pragma solidity ^0.4.18;
+
+import "./EIP20Interface.sol";
+
+contract EIP521Interface is EIP20Interface {
+ 
+  function mint(uint256 nonce, bytes32 challenge_digest) public returns (bool success);
+
+  function getChallengeNumber() public constant returns (bytes32);
+
+  function getMiningDifficulty() public constant returns (uint);
+
+  function getMiningTarget() public constant returns (uint);
+
+  function getMiningReward() public constant returns (uint);
+
+}
