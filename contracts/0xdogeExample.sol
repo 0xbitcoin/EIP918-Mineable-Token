262e1da77f96e05de741f0807fd09a36eaceb401
Jay Logelin <jason.logelin@virtustream.com>
2018-03-23 15:01:19 -0300

    Added missing pragma statement in example contract

--- contracts/0xdogeExample.sol
+++ contracts/0xdogeExample.sol
@@ -1,4 +1,4 @@
-
+pragma solidity ^0.4.18;
 import "./0xBitcoinBase.sol";
-/* Two liner to create a 0xb based mineable token */
+/* Three liner to create a 0xb based mineable token */
 contract _0xdoge is _0xBitcoinBase('0xDoge', '0xDoge Mineable Token', 113000000000, 12, 200, 512) { }
