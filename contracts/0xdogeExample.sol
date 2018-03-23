558c20062ef98b0cb0e112cf7e845abe50fdcf3a
Jay Logelin <jason.logelin@virtustream.com>
2018-03-23 14:27:44 -0300

    Added example 0xb based token

---
+++ contracts/0xdogeExample.sol
@@ -0,0 +1,4 @@
+
+import "./0xBitcoinBase.sol";
+/* Two liner to create a 0xb based mineable token */
+contract _0xdoge is _0xBitcoinBase('0xDoge', '0xDoge Mineable Token', 113000000000, 12, 200, 512) { }
