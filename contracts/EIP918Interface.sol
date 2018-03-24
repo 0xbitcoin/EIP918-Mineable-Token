9ffa97cedbb21fd3ed01f6edc5bd8ea34f48ae66
Jay Logelin <jason.logelin@virtustream.com>
2018-03-23 23:37:54 -0300

    Refactored abstract methods into interface

--- contracts/EIP918Interface.sol
+++ contracts/EIP918Interface.sol
@@ -1,21 +1,37 @@
 // Abstract contract for the full ERC 20 Token standard
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 pragma solidity ^0.4.18;
- 
 
 interface EIP918Interface  {
 
-  function mint(uint256 nonce, bytes32 challenge_digest) public returns (bool success);
+  	function mint(uint256 nonce, bytes32 challenge_digest) public returns (bool success);
 
-  function getChallengeNumber() public constant returns (bytes32);
+	/*
+     * Returns the challenge number
+     **/
+    function getChallengeNumber() public constant returns (bytes32);
 
-  function getMiningDifficulty() public constant returns (uint);
+    /*
+     * Returns the mining difficulty. The number of digits that the digest of the PoW solution requires which 
+     * typically auto adjusts during reward generation.
+     **/
+    function getMiningDifficulty() public constant returns (uint);
 
-  function getMiningTarget() public constant returns (uint);
+    /*
+     * Returns the mining target
+     **/
+    function getMiningTarget() public constant returns (uint);
 
-  function getMiningReward() public constant returns (uint);
- 
-  event Mint(address indexed from, uint reward_amount, uint epochCount, bytes32 newChallengeNumber);
-
+    /*
+     * Return the current reward amount. Depending on the algorithm, typically rewards are divided every reward era 
+     * as tokens are mined to provide scarcity
+     **/
+    function getMiningReward() public constant returns (uint);
+    
+    /*
+     * Upon successful verification and reward the mint method dispatches a Mint Event indicating the reward address, 
+     * the reward amount, the epoch count and newest challenge number.
+     **/
+    event Mint(address indexed from, uint reward_amount, uint epochCount, bytes32 newChallengeNumber);
 
 }
